7ad36195b121ae2a53c10b9e255270f1d500b191
Ravindra <31267538+ravindrababuweblogic@users.noreply.github.com>
2024-03-01 00:25:53 +0530

    Create app.js
    initial file1

---
+++ .github/workflows/node-my-app/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log('Server listening at http://13.66.138.105:3000');
