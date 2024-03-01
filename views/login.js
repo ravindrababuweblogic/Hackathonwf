32d4639da98d262c8245b199a674c96e6bd55f97
Ravindra <31267538+ravindrababuweblogic@users.noreply.github.com>
2024-03-01 10:33:33 +0530

    Create login.js

---
+++ views/login.js
@@ -0,0 +1,4 @@
+<form action="/login" method="post">
+  <div class="form-group">
+    <label for="email">Email:</label>
+    <input type="email" class="form-control" name="email" id="email" required>
